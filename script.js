ed66e19eb5b63657a5f86f3d034e7ea422294ede
Rozaline Kozly <rozaline.ko@gmail.com>
2025-06-25 19:55:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 const supabaseUrl = 'https://ldignxlbttqoqcvoycbj.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxkaWdueGxidHRxb3Fjdm95Y2JqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA4NTk3NDcsImV4cCI6MjA2NjQzNTc0N30.6kK3C9SI6GCqggzpf8qIPB31_PMIvs3ToPRpkjZFdII'
-
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 window.fetchMessage = async function () {
@@ -10,13 +9,12 @@ window.fetchMessage = async function () {
   if (!id) return alert('Please enter a number.')
 
   const { data, error } = await supabase
-    .from('messages')
+    .from('messages') // table name
     .select('my_msg')
     .eq('id', id)
     .single()
 
   if (error || !data) {
-    console.error(error)
     alert('Roza: Message not found.')
     return
   }
@@ -35,7 +33,6 @@ window.updateMessage = async function () {
     .eq('id', id)
 
   if (error) {
-    console.error(error)
     alert('Update failed.')
   } else {
     alert('Message updated!')
